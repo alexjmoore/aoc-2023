bd0acbc50c7be7872ce8a5ab62a9e641f503ec90
Alex Moore <alex@moore.mx>
2023-12-01 19:34:04 +0000

    tidy up

--- 01/01b.py
+++ 01/01b.py
@@ -14,26 +14,18 @@ conversion = {
 
 PATTERN = r'^one|two|three|four|five|six|seven|eight|nine'
 
-total = 0
-
 def checkpos(s,n):
-    if s[0].isdigit():
-        return int(line[0])
-    else:
-        m = re.match(PATTERN, s)
-        if m:
-            return(conversion[m.group(0)])
+    if s[0].isdigit(): return int(line[0])
+    if m := re.match(PATTERN, s): return(conversion[m.group(0)])
     return n
 
+total = 0
 with open('input.txt') as input:
     while line := input.readline().strip():
-        first = last = 0
+        first = last = None
         while len(line) > 0:
-            if first == 0:
-                first = checkpos(line, first)
+            if not first: first = checkpos(line, first)
             last = checkpos(line, last)
-            line = line[1:]
-        
+            line = line[1:]        
         total += int(f"{first}{last}")
-
 print(f"total: {total}")
